b687a875598169053d884c31c4f2f8f9e73a0162
Kent Slaney <kent@slaney.org>
2025-10-04 15:33:48 -0700

    build for a sandboxed powershell wrapper

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-all:
-	mkdir -p build/include;\
+all: inject sandbox
+
+inject:
+	mkdir -p build/include dist;\
 	[ ! -d build/BakkesModInjectorCpp ] && git clone https://github.com/bakkesmodorg/BakkesModInjectorCpp.git build/BakkesModInjectorCpp;\
 	git apply -q --directory=build/BakkesModInjectorCpp mingw.patch;\
 	ln -sf /usr/x86_64-w64-mingw32/include/windows.h "build/include/Windows.h";\
@@ -11,7 +13,16 @@ all:
         "build/BakkesModInjectorCpp/BakkesModInjectorC++/WindowsUtils.cpp" \
         "build/BakkesModInjectorCpp/BakkesModWPF/BakkesModWPF.cpp" \
         "build/BakkesModInjectorCpp/BakkesModInjectorC++/DllInjector.cpp" \
-        "main.cpp" -o "build/inject.exe"
+        "main.cpp" -o "dist/inject.exe"
+
+sandbox:
+	mkdir -p build dist;\
+	[ ! -d build/powershell-wrapper-for-wine ] && git clone https://github.com/PietJankbal/powershell-wrapper-for-wine.git build/powershell-wrapper-for-wine;\
+	git apply -q --directory=build/powershell-wrapper-for-wine sandbox.patch;\
+	x86_64-w64-mingw32-gcc -O1 -fno-ident -fno-stack-protector -fomit-frame-pointer -fno-unwind-tables -fno-asynchronous-unwind-tables -falign-functions=1 -falign-jumps=1 -falign-loops=1 -fwhole-program \
+		-mconsole -municode -mno-stack-arg-probe -Xlinker --stack=0x200000,0x200000 -nostdlib  -Wall -Wextra -ffreestanding build/powershell-wrapper-for-wine/main.c -lurlmon -lkernel32 -lucrtbase -nostdlib -lshell32 -lshlwapi -s -o dist/powershell64.exe;\
+	i686-w64-mingw32-gcc   -O1 -fno-ident -fno-stack-protector -fomit-frame-pointer -fno-unwind-tables -fno-asynchronous-unwind-tables -falign-functions=1 -falign-jumps=1 -falign-loops=1 -fwhole-program \
+		-mconsole -municode -mno-stack-arg-probe -Xlinker --stack=0x200000,0x200000 -nostdlib  -Wall -Wextra -ffreestanding build/powershell-wrapper-for-wine/main.c -lurlmon -lkernel32 -lucrtbase -nostdlib -lshell32 -lshlwapi -s -o dist/powershell32.exe
 
 clean:
-	rm -rf build
+	rm -rf build dist
---
+++ sandbox.py
@@ -0,0 +1,54 @@
+import sys, http.server
+from urllib.parse import urlparse, parse_qs
+
+urls = {
+    "https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/PowerShell-7.4.1-win-x64.msi": "application/octet-stream",
+    "https://github.com/Maximus5/ConEmu/releases/download/v23.07.24/ConEmuPack.230724.7z": "application/octet-stream",
+    "https://www.7-zip.org/a/7zr.exe": "application/octet-stream",
+    "https://raw.githubusercontent.com/PietJankbal/powershell-wrapper-for-wine/master/profile.ps1": "text/plain; charset=utf-8"
+}
+
+class SourcesSandbox(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = parse_qs(urlparse(self.path).query)
+        try:
+            url = next(iter(query.get("url", [])))
+        except StopIteration:
+            self.send_error(400)
+            return
+        if url not in urls:
+            self.send_error(404)
+            return
+        fname = url.rsplit("/", 1)[-1]
+        with open(fname, 'rb') as fp:
+            content = fp.read()
+
+        self.send_response(200)
+        self.send_header('content-type', urls[url])
+        self.send_header('content-length', str(len(content)))
+        self.send_header('content-disposition', f'attachment; filename={fname}')
+        self.end_headers()
+        self.wfile.write(content)
+
+widen = lambda x: bytes(sum(([i, 0] for i in x), []))
+placeholder = widen(b'http://127.0.0.1:00000?pad=&url=')
+
+def padded(port):
+    assert 0 < int(port) <= 65_535
+    pad = 5 - len(str(port))
+    return widen(f"http://127.0.0.1:{port}?pad={'-' * pad}&url=".encode())
+
+def patch(port):
+    for arch in [32, 64]:
+        with open(f"powershell{arch}.exe", 'rb') as fp:
+            f = fp.read()
+        with open(f"powershell{arch}_.exe", 'wb') as fp:
+            fp.write(f.replace(placeholder, padded(port)))
+
+def run_server():
+    httpd = http.server.HTTPServer(('', 0), SourcesSandbox)
+    patch(httpd.server_address[1])
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
