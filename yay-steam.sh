6a44cebb4a084d70d11e99ab9f321c2bad8352dd
Kent Slaney <kent@slaney.org>
2025-12-25 16:08:43 -0800

    Jguer/yay#2508

--- yay-steam.sh
+++ yay-steam.sh
@@ -6,3 +6,8 @@ sudo pacman --noconfirm -S git base-devel
 cd yay-bin
 git pull
 makepkg --noconfirm -si
+
+# https://github.com/Jguer/yay/issues/2508#issuecomment-3657461690
+linked_so='/lib/'`ldd "$(which yay)" | grep -o 'libalpm.\S* ' | head -1`
+existing_so='/lib/libalpm.so'
+[ ! -e "$linked_so" ] && [ -f "$existing_so" ] && sudo ln -s "$existing_so" "$linked_so"
